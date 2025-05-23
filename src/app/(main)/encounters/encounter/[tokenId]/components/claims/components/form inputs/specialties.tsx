e54c9226f225f3ccb742855f464d7feab8f8f65e
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2025-05-23 08:43:00 +0000

    Upade tooltip for specialties attended

--- src/app/(main)/encounters/encounter/[tokenId]/components/claims/components/form inputs/specialties.tsx
+++ src/app/(main)/encounters/encounter/[tokenId]/components/claims/components/form inputs/specialties.tsx
@@ -4,6 +4,8 @@ import Text from "@styles/components/text";
 import theme from "@styles/theme";
 import { TypographyBold } from "@styles/style.types";
 import { useEffect, useState } from "react";
+import { FiInfo } from "react-icons/fi";
+import { hexOpacity } from "@/utils/hexOpacity";
 
 export type IOptions = 'ASUR' | 'DENT' | 'ENTH' | 'MEDI' | 'OBGY' | 'OPDC' | 'OPHT' | 'ORTH' | 'PAED' | 'PSUR' | 'RSUR';
 
@@ -21,6 +23,35 @@ const tooltipTexts: Record<IOptions, string> = {
     RSUR: "Reconstructive Surgery"
 };
 
+const SpecialtiesTooltip = ({
+    text
+}: {
+    text: string
+}) => {
+    const [isHovered, setIsHovered] = useState(false);
+    return (
+        <Text>
+            <div
+                className="flex items-center gap-1"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                {text}
+                <Tooltip
+                    title={tooltipTexts[text as IOptions]}
+                    placement="top"
+                >
+                    <FiInfo
+                        color={theme.colors.main.primary + hexOpacity(isHovered ? 70 : 0)}
+                        size={14}
+                        className="mt-[-2px]"
+                    />
+                </Tooltip>
+            </div>
+        </Text>
+    )
+}
+
 const Specialties = () => {
     const { formik } = useClaimsFormContext();
     const [selectedOption, setSelectedOption] = useState<IOptions[]>(formik.values.specialties as IOptions[]);
@@ -34,16 +65,7 @@ const Specialties = () => {
 
     const options: CheckboxOptionType<IOptions>[] = Object.keys(tooltipTexts).map((key) => ({
         label: (
-            <div className="flex items-center gap-2">
-                <Text>
-                    <Tooltip
-                        title={tooltipTexts[key as IOptions]}
-                        placement="top"
-                    >
-                        {key}
-                    </Tooltip>
-                </Text>
-            </div>
+            <SpecialtiesTooltip text={key} />
         ),
         value: key as IOptions
     }));
